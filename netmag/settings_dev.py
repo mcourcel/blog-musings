71d3cc16b2cc44180a8ace302f3ad77c9ade4f29
Martin Courcelles <mcourcel@gmail.com>
2013-02-14 23:17:39 -0500

    fixed static tuple and removed nose from requirements

--- netmag/settings_dev.py
+++ netmag/settings_dev.py
@@ -38,7 +38,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'c:/test/netmag/netmag/static'
+	'c:/test/netmag/netmag/static',
 )
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
